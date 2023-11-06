bbc49896de11d24fc12b9fa538507fb3fca39c89
Michel Van Hoof <m-vanhoof@lbctt.com>
2023-11-06 12:13:23 +0100

    Add of software installer scripts

---
+++ Scripts/Install-Default-Applications.ps1
@@ -0,0 +1,17 @@
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco upgrade -y
+choco install -y googlechrome
+choco install -y greenshot
+choco install -y adobereader
+choco install -y vlc
+choco install -y firefox
+choco install -y office365business
+choco install -y mremoteng
+choco install -y azure-cli
+choco install -y vscode
+choco install -y spotify
+choco install -y vmware-workstation-player
+choco install -y whatsapp
+choco install -y putty
+choco install -y 7zip
---
+++ SystemScripts/software-installer.bat
@@ -0,0 +1,6 @@
+choco install googlechrome
+choco install adobereader
+choco install firefox
+choco install vscode
+
+
